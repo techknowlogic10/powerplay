0f6ef5cb9bfaa5170b7b6c7402713fd83f5e4a15
anuradhanamala <anuradha.namala@gmail.com>
2022-08-14 12:03:08 -0400

    FirstProgram

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstProgram.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Autonomous
+public class FirstProgram extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        telemetry.log().add("first program");
+        sleep(3000);
+
+    }
+}
