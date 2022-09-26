ce719256abdd6f74f8e837cc62f44ba7740e62fc
Vishal <vishallokesh09@gmail.com>
2022-09-26 18:26:03 -0400

    updated drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MacnumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MacnumTeleOp.java
@@ -22,7 +22,6 @@ public class MacnumTeleOp extends OpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
 
         frontRight = hardwareMap.dcMotor.get("frontRight");
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         backRight = hardwareMap.dcMotor.get("backRight");
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -31,14 +30,14 @@ public class MacnumTeleOp extends OpMode {
     @Override
     public void loop() {
 
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double y = gamepad1.left_stick_y; // Remember, this is reversed!
         double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio, but only when
         // at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), .25);
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
