3e2c54a640352590f5592dcf0abbcc0a295ee2d7
techknowlogic10 <89231531+techknowlogic10@users.noreply.github.com>
2022-10-12 19:20:41 -0400

    More auton code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.9685; // in
     public static double GEAR_RATIO = 1.6; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.6; // in
+    public static double TRACK_WIDTH = 16.43; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -56,9 +56,9 @@ public class DriveConstants {
      * TODO tune the robot and find the optimal values for kV, kA and kStatic
      */
     //public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kV = 0.0081;
-    public static double kA = 0.001;
-    public static double kStatic = 0.1445;
+    public static double kV = 0.0099;
+    public static double kA = 0.002;
+    public static double kStatic = 0.1;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -69,7 +69,7 @@ public class DriveConstants {
      *
      * TODO check these values
      */
-    public static double MAX_VEL = 30;
+    public static double MAX_VEL = 24;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
